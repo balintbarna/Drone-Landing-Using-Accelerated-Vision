2a1af97241475423f169031ac8704a830b77d852
BaLint Kovari <balintmaci@gmail.com>
2021-04-01 19:07:47 +0000

    restructure project

--- dai_jupyter/daipy/postproc.py
+++ dai_jupyter/daipy/postproc.py
@@ -4,7 +4,7 @@ def calculate_softmax(data):
     result = np.exp(data)
     return result
 
-def predict_label(softmax):
-    with open("words.txt", "r") as f:
+def predict_label(softmax, path):
+    with open(path, "r") as f:
         lines = f.readlines()
     return lines[np.argmax(softmax)-1]
