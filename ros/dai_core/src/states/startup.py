c92f4a11329bd5ad8544855c1d797aed98b5ccc9
Balint Kovari <balintmaci@gmail.com>
2021-05-19 11:14:49 +0000

    import move in startup

--- ros/dai_core/src/states/startup.py
+++ ros/dai_core/src/states/startup.py
@@ -1,10 +1,12 @@
-from states.wait_for_control import WaitForControl
+import rospy
 
 class Startup:
     def __init__(self, sm):
         pass
 
     def execute(self, sm):
+        rospy.logout("execute startup")
         if sm.mav.connected():
             sm.mav.start()
+            from states.wait_for_control import WaitForControl
             sm.set_state(WaitForControl(sm))
